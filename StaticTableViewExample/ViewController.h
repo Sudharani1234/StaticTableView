df4b6cf03e72afa2c294efabae4f702cab817245
Sudharani1234 <sudharanipb075@gmail.com>
2017-12-30 13:40:16 +0530

    Initial commit

---
+++ StaticTableViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticTableViewExample
+//
+//  Created by Vijay on 03/03/17.
+//  Copyright © 2017 Sudha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StaticTableViewExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StaticTableViewExample
+//
+//  Created by Vijay on 03/03/17.
+//  Copyright © 2017 Sudha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
